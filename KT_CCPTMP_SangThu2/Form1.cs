11d44b963fcfa8426babdfef18e08cc1da7a5e11
Tran Nhat Anh Thuan <anhthuan06042003@gmail.com>
2025-03-03 09:28:00 +0700

    add chức năng đăng nhập

--- KT_CCPTMP_SangThu2/Form1.cs
+++ KT_CCPTMP_SangThu2/Form1.cs
@@ -16,5 +16,21 @@ namespace KT_CCPTMP_SangThu2
         {
             InitializeComponent();
         }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            if (textboxUsername.Text == "admin" && textboxMatkhau.Text == "admin")
+            {
+                MessageBox.Show("Đăng nhập thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                HomePage homePage = new HomePage(); // Tạo instance của HomePage
+                homePage.Show(); // Hiển thị HomePage
+                this.Hide(); // Ẩn form đăng nhập
+            }
+            else
+            {
+                MessageBox.Show("Sai tên đăng nhập hoặc mật khẩu!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
